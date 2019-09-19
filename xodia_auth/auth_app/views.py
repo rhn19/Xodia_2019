60069cacc61177d4299110756773bb95b63abc30
Rohan Bhide <rohanbhide19@hotmail.com>
2019-09-19 19:20:17 +0530

    Upload fns added

--- xodia_auth/auth_app/views.py
+++ xodia_auth/auth_app/views.py
@@ -3,18 +3,25 @@ from __future__ import unicode_literals
 
 from django.shortcuts import render
 from django.contrib.auth import authenticate, login, logout
-from django.http import HttpResponseRedirect
+from django.http import HttpResponseRedirect, HttpResponse
 from django.urls import reverse
-from models import User
+from .models import User
 from .models import Profile
 from django.views.generic import View
 from django.db.utils import IntegrityError
 import urllib2
 import urllib
 import json
+import os
 from django.conf import settings
+from django.core.files.storage import FileSystemStorage
+from .__init__ import project_base_dir
+
 
 # Create your views here.
+
+match_path = project_base_dir + "matches/"
+bot_path = project_base_dir + "files/"
 
 
 class LoginView(View):
@@ -22,7 +29,7 @@ class LoginView(View):
 
     def get(self, request):
         if request.user.is_authenticated():
-            #user = request.user.username
+            # user = request.user.username
             # return render(request, 'auth_app/postlogin.html', {'username': user})
             return HttpResponseRedirect(reverse('login_success'))
         else:
@@ -69,7 +76,7 @@ class RegisterView(View):
 
     def get(self, request):
         if request.user.is_authenticated():
-            #user = request.user.username
+            # user = request.user.username
             # return render(request, 'auth_app/postlogin.html', {'username': user})
             return HttpResponseRedirect(reverse('login_success'))
         else:
@@ -117,3 +124,104 @@ class PlayableUI(View):
     def get(self, request):
         au = request.user.is_authenticated
         return render(request, self.template_name, {'au': au})
+
+
+class GridView(View):
+    template_name = 'auth_app/buttons.html'
+
+    def get(self, request):
+        if request.user.is_authenticated:
+            all_players = Profile.objects.all()
+            return render(request, self.template_name, {'all_players': all_players})
+        else:
+            return HttpResponseRedirect(reverse('user_login'))
+
+    def post(self, request):
+
+        if request.FILES:
+            return HttpResponse(self.uploadBot(request))
+        elif 'Match' in request.POST:
+            return HttpResponse(self.matchGame(request))
+        elif 'Logs' in request.POST:
+            return HttpResponse(self.viewLogs(request))
+        else:
+            return HttpResponse('upload a file jackass')
+
+    def uploadBot(self, request):
+        player = Profile.objects.get(user=request.user)
+
+        play = request.FILES['Bot_file']
+
+        ext = play.name.split('.')[-1]
+
+        if ext in ['cpp','c']:
+            old_file = player.bot_path + '.' + player.bot_ext
+
+            os.remove(old_file)
+
+            new_file = player.bot_path + '.' + ext
+            f = open(new_file, 'w+')
+
+            for chunk in play.chunks():
+                f.write(chunk)
+            f.close()
+
+            player.bot_ext = ext
+
+            player.save()
+            return '<h1>' + str(play.name) + '</h1>'
+
+        else:
+            return HttpResponse('error')
+
+    def matchGame(self, request):
+        player = Profile.objects.get(user=request.user)
+        temp = User.objects.get(username = request.POST['players'])
+        opp = Profile.objects.get(user = temp)
+
+        error_file = open(("matches/" + "error" + str(player.pk) + "v" + str(opp.pk)), "w+")
+        error_file.close()
+        error_rev_file = open(("matches/" + "error" + str(opp.pk) + "v" + str(player.pk)), "w+")
+        error_rev_file.close()
+
+        log_file = open(("matches/" + "log" + str(player.pk) + "v" + str(opp.pk)), "w+")
+        log_file.close()
+        log_rev_file = open(("matches/" + "log" + str(opp.pk) + "v" + str(player.pk)), "w+")
+        log_rev_file.close()
+
+        return HttpResponse(str(player.bot_path)+'<br>'+str(opp.bot_path))
+
+    def viewLogs(self, request):
+        player = Profile.objects.get(user=request.user)
+        temp = User.objects.get(username = request.POST['players'])
+        opp = Profile.objects.get(user = temp)
+
+        if 'Player2' not in request.POST:
+            try:
+                error_file = open(("matches/" + "error" + str(player.pk) + "v" + str(opp.pk)), "r")
+                try:
+                    log_file = open(("matches/" + "log" + str(player.pk) + "v" + str(opp.pk)), "r")
+                    log_file.close()
+                except:
+                    print("Log File Not Found!")
+                    return
+                error_file.close()
+            except:
+                print("Error File Not Found!")
+                return
+            return HttpResponse("exists1")
+
+        else:
+            try:
+                error_rev_file = open(("matches/" + "error" + str(opp.pk) + "v" + str(player.pk)), "r")
+                try:
+                    log_rev_file = open(("matches/" + "log" + str(opp.pk) + "v" + str(player.pk)), "r")
+                    log_rev_file.close()
+                except:
+                    print("Log File Not Found!")
+                    return
+                error_rev_file.close()
+            except:
+                print("Error File Not Found!")
+                return
+            return HttpResponse("exists2")
